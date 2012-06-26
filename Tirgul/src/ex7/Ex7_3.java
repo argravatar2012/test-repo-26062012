0bf21aa95101a4559eef91838d2630cc9a1f4a87
Assaf Classroom <assaf.gannon@classroom.com>
2012-06-26 20:55:08 +0300

    another commit

--- Tirgul/src/ex7/Ex7_3.java
+++ Tirgul/src/ex7/Ex7_3.java
@@ -23,7 +23,29 @@ public class Ex7_3 {
 		
 		System.out.println(counter);
 		
+		int b[] = new int[counter];		
 		
+		int k=1; //current index of b array
+		b[0] = a[0]; //assign the first - always unique
+		boolean found = false; //Will be used to indicate if the number exists
+		for(int i=1;i<a.length;i++){ //Start from 1  - 0 is already set
+			int num = a[i]; //get the current number to check
+			for(int j=k-1;j>=0;j--){
+				if(b[j]==num){
+					found = true;
+					break;
+				}
+			}
+			if(!found){
+				b[k] = num;
+				k++;
+			}
+			found = false;
+		}
+		
+		for (int i = 0; i < b.length; i++) {
+			System.out.print(b[i]+"\t");
+		}
 		
 	}
 	
